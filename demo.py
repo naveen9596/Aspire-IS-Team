c1617a3ffbc44eb91910fef99947e3a40f896d0e
Naveenkumar <naveen.subramaniam@aspiresys.com>
2024-05-15 14:33:51 +0530

    First Commit

---
+++ demo.py
@@ -0,0 +1,5 @@
+# Example: vowels list
+
+list = ['e', 'a', 'u', 'o', 'i']
+print(sorted(list))                # ▶ ['a', 'e', 'i', 'o', 'u']
+print(sorted(list, reverse=True))  # ▶ ['u', 'o', 'i', 'e', 'a']
